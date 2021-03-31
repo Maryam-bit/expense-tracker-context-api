09acc35d10f661c826878dd9d9fda749e955578f
Maryam Noor <rajputmaryam5555@gmail.com>
2021-04-01 00:55:23 +0500

    expense tracker using context api and reducer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Header from './component/header/header.component'
+import Balance from './component/balance/balance.component'
+import IncomeExpense from './component/income-expense/income-expense.component'
+import AddTransaction from './component/add-transaction/add-transaction.component'
+import TransactionHistory from './component/transaction-history/transaction-history.component'
+import {GlobalProvider} from './context/global-state'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GlobalProvider>
+      <Header />
+      <Balance />
+
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <div className="add-tranaction-component">
+              <IncomeExpense />
+              <AddTransaction />
+            </div>
+          </div>
+          <div className="col-md-6">
+            <TransactionHistory />
+          </div>
+        </div>
+      </div>
+
+    </GlobalProvider>
   );
 }
 
